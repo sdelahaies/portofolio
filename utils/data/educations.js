c0632a8af97d82552f01c7f2d70f5233ad815c16
sdelahaies <sdelahaies@gmail.com>
2024-12-23 14:09:46 +0100

    feat: adding IAssistant blog-data

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -24,24 +24,3 @@ export const educations = [
     institution: "Université d'Angers",
   }
 ]
-
-// export const educations = [
-//   {
-//     id: 1,
-//     title: "Bachelor Degree",
-//     duration: "2020 - Present",
-//     institution: "National University of Bangladesh",
-//   },
-//   {
-//     id: 2,
-//     title: "Higher Secondary Certificate",
-//     duration: "2018 - 2020",
-//     institution: "Noakhali Islamia Kamil Madrasah",
-//   },
-//   {
-//     id: 3,
-//     title: "Secondary School Certificate",
-//     duration: "2008 - 2018",
-//     institution: "Baitus Saif Islamia Madrasah",
-//   }
-// ]
