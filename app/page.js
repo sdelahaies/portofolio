b77a64eae249113427407edcea300a196d0bd7c2
sdelahaies <sdelahaies@gmail.com>
2025-04-10 09:29:33 +0200

    add HTR project

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
+// import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 import Research from "./components/homepage/research";
 
